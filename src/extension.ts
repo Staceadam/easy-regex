a01103c89aa24b469e9227ff9afc75e788acae51
KEL2649 <stacykelnhofer@northwesternmutual.com>
2023-08-11 11:15:39 -0500

    try catch for json parse

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from 'openai'
 import 'dotenv/config'
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+	apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration)
@@ -11,91 +11,97 @@ const openai = new OpenAIApi(configuration)
 const MODEL = 'gpt-3.5-turbo'
 
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand('easy-regex.easyRegex', async () => {
-        // Get the active text editor
-        const editor = vscode.window.activeTextEditor
+	const disposable = vscode.commands.registerCommand('easy-regex.easyRegex', async () => {
+		// Get the active text editor
+		const editor = vscode.window.activeTextEditor
 
-        if (editor) {
-            const selection = editor.selection
+		if (editor) {
+			const selection = editor.selection
 
-            if (!selection || selection.isEmpty) {
-                vscode.window.showErrorMessage('You must select something')
-            }
+			if (!selection || selection.isEmpty) {
+				vscode.window.showErrorMessage('You must select something')
+			}
 
-            const selectionRange = new vscode.Range(
-                selection.start.line,
-                selection.start.character,
-                selection.end.line,
-                selection.end.character
-            )
+			const selectionRange = new vscode.Range(
+				selection.start.line,
+				selection.start.character,
+				selection.end.line,
+				selection.end.character
+			)
 
-            const highlighted = editor.document.getText(selectionRange)
+			const highlighted = editor.document.getText(selectionRange)
 
-            const searchQuery = await vscode.window.showInputBox({
-                placeHolder: 'What are you trying to match?',
-                prompt: 'Enter Search Query'
-            })
+			const searchQuery = await vscode.window.showInputBox({
+				placeHolder: 'What are you trying to match?',
+				prompt: 'Enter Search Query'
+			})
 
-            if (searchQuery === '') {
-                vscode.window.showErrorMessage('A search query is mandatory to execute this action')
-            }
+			if (searchQuery === '') {
+				vscode.window.showErrorMessage('A search query is mandatory to execute this action')
+			}
 
-            if (searchQuery !== undefined) {
-                vscode.window.showInformationMessage(`Searching for ${searchQuery}...`)
+			if (searchQuery !== undefined) {
+				vscode.window.showInformationMessage(`Searching for ${searchQuery}...`)
 
-                const content =
-                    process.env.QUERY_START + `${searchQuery} in this string '${highlighted}'` + process.env.QUERY_END
+				const content =
+					process.env.QUERY_START + `${searchQuery} in this string '${highlighted}'` + process.env.QUERY_END
 
-                const chatCompletion = await openai.createChatCompletion({
-                    model: MODEL,
-                    messages: [{ role: 'user', content }]
-                })
+				const chatCompletion = await openai.createChatCompletion({
+					model: MODEL,
+					messages: [{ role: 'user', content }]
+				})
 
-                const final = chatCompletion.data.choices[0]?.message?.content
+				const final = chatCompletion.data.choices[0]?.message?.content
 
-                if (final) {
-                    const regexObj = JSON.parse(final)
+				if (final) {
+					try {
+						const regexObj = JSON.parse(final)
 
-                    vscode.window.showInformationMessage(regexObj.regex)
+						vscode.window.showInformationMessage(regexObj.regex)
 
-                    const decorationType = vscode.window.createTextEditorDecorationType({
-                        backgroundColor: 'green',
-                        border: '1px solid white'
-                    })
+						const decorationType = vscode.window.createTextEditorDecorationType({
+							backgroundColor: 'green',
+							border: '1px solid white'
+						})
 
-                    let decorationsArray: vscode.DecorationOptions[] = []
-                    const lines = highlighted.split('\n')
+						let decorationsArray: vscode.DecorationOptions[] = []
+						const lines = highlighted.split('\n')
 
-                    for (let line = 0; line < lines.length; line++) {
-                        let matches = lines[line].matchAll(regexObj.regex)
+						for (let line = 0; line < lines.length; line++) {
+							let matches = lines[line].matchAll(regexObj.regex)
 
-                        const matchAll = [...matches]
+							const matchAll = [...matches]
 
-                        matchAll.forEach((match) => {
-                            if (match !== null && match.index !== undefined) {
-                                let range = new vscode.Range(
-                                    new vscode.Position(line, match.index),
-                                    new vscode.Position(line, match.index + match[0].length)
-                                )
+							matchAll.forEach((match) => {
+								if (match !== null && match.index !== undefined) {
+									let range = new vscode.Range(
+										new vscode.Position(line, match.index),
+										new vscode.Position(line, match.index + match[0].length)
+									)
 
-                                let decoration = { range }
+									let decoration = { range }
 
-                                decorationsArray.push(decoration)
-                            }
-                        })
-                    }
+									decorationsArray.push(decoration)
+								}
+							})
+						}
 
-                    editor.setDecorations(decorationType, decorationsArray)
+						editor.setDecorations(decorationType, decorationsArray)
 
-                    vscode.workspace.onDidChangeTextDocument((e) => {
-                        decorationType.dispose()
-                    })
-                }
-            }
-        }
-    })
+						vscode.workspace.onDidChangeTextDocument((e) => {
+							decorationType.dispose()
+						})
+					} catch (error) {
+						vscode.window.showWarningMessage(
+							`Did not find a result for "${searchQuery}". Please try again or update the query and try again.`
+						)
+					}
+				}
+			}
+		}
+	})
 
-    context.subscriptions.push(disposable)
+	context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
